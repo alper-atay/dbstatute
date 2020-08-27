27dc8ed3f1f1acb10557d08315a3e2c60d44d012
Alper Atay <aaechoman@gmail.com>
2020-08-27 18:28:35 +0300

    Create SingleInsertByRawModel.cs

---
+++ DbStatute/DbStatute/Singles/SingleInsertByRawModel.cs
@@ -0,0 +1,22 @@
+﻿using DbStatute.Fundamentals.Singles;
+using DbStatute.Interfaces;
+using DbStatute.Interfaces.Singles;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace DbStatute.Singles
+{
+    public class SingleInsertByRawModel<TModel> : SingleInsertBase<TModel>, ISingleInsertByRawModel<TModel>
+        where TModel : class, IModel, new()
+    {
+        public TModel RawModel { get; }
+
+        object IRawModel.RawModel => RawModel;
+
+        protected override Task<TModel> InsertOperationAsync(IDbConnection dbConnection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
